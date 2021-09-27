b662c11752ecf3b4d273edb447af46f36bc64acf
PKRMSEZDC\lvidyasagar <lvidyasagar@pkglobal.com>
2021-09-27 17:39:24 +0530

    latest changes

--- apps/booksapi/src/environments/environment.prod.ts
+++ apps/booksapi/src/environments/environment.prod.ts
@@ -8,5 +8,14 @@ export const environment = {
     'mongodb+srv://user1:admin@cluster0.b0b4l.mongodb.net/booksapi?authSource=admin&replicaSet=atlas-u4gud8-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true',
   logDir: process.env.LOG_DIR || 'logs',
   logLevel: process.env.LOG_LEVEL || 'info',
-  logFile: process.env.LOG_FILE || 'app.log'
+  logFile: process.env.LOG_FILE || 'app.log',
+  ORG_URL: process.env.ORG_URL || 'https://dev-68266919.okta.com',
+  ISSUER: process.env.ISSUER || 'https://dev-68266919.okta.com/oauth2/default',
+  CLIENT_ID: process.env.CLIENT_ID || '0oa1wvzk56rGwNEe65d7',
+  CLIENT_SECRET:
+    process.env.CLIENT_SECRET || 'XYDXuIT4Iz3HyQ5RTIfEc1bFyS0lEcrbaHEFfFK9',
+  SCOPE: 'openid profile offline_access',
+  API_TOKEN:
+    process.env.API_TOKEN || '00KUrGmvxs6coTHKzezZ9UPRsj7NkC1bDmLCcgcABJ',
+  APP_BASE_URL: 'http://localhost:3000',
 };
